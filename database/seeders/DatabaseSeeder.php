a03d8b563a3feafe8cd33cd05091cf5668237c72
Vidal1254Pa <58918751+Vidal1254Pa@users.noreply.github.com>
2024-09-12 20:38:47 -0500

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        $roles = [
+            ['name' => 'SUPERADMIN ADMIN', 'id' => 1],
+            ['name' => 'VENDEDOR', 'id' => 2],
+        ];
+
+        foreach ($roles as $role) {
+            DB::table('roles')->insert($role);
+        }
+
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'role_id' => 1,
+        ]);
+    }
+}
